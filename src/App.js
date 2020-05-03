11152ce38b1390c175cdc84683d0c5f4f1bb9beb
johnbarguti <jaadbarg@gmail.com>
2020-05-02 22:24:54 -0400

    react test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos'
+
+var state = {
+  todos: [ { 
+    id: 1,
+    title: "eat cheese",
+    done: false
+  },
+  { 
+    id: 1,
+    title: "eat pie",
+    done: false
+  },
+  { 
+    id: 1,
+    title: "eat fish",
+    done: false
+  }
+]
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return ( 
+
+    <Todos todos={state}/>
+
   );
 }
 
