ea2a423c06f9e7b6467c9ef4dfb04e8749546f99
johnbarguti <jaadbarg@gmail.com>
2020-05-13 17:33:55 -0400

    Updates

---
+++ precache-manifest.974188c8b7aa783ba09df1f182c52fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bc15432517e795e1de917dcb0ed08b",
+    "url": "/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "a4df776a8f2eb5c53404",
+    "url": "/ToDoReactApp/static/css/main.bc85319c.chunk.css"
+  },
+  {
+    "revision": "403c8eb0ac3e555ceb5b",
+    "url": "/ToDoReactApp/static/js/2.f2835cd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoReactApp/static/js/2.f2835cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4df776a8f2eb5c53404",
+    "url": "/ToDoReactApp/static/js/main.1fb6a8d0.chunk.js"
+  },
+  {
+    "revision": "b66cbb540ae38845acdb",
+    "url": "/ToDoReactApp/static/js/runtime-main.711f2d3f.js"
+  }
+]);
